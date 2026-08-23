e7728f184965bf7112e98e55c7c862ef1fc5d9a3
Serge <sajghaa@gmail.com>
2026-08-23 23:09:09 +0200

    Added our first coded in C sharp

--- src/Fundamentals/IronSharp.Fundamentals.Console/Program.cs
+++ src/Fundamentals/IronSharp.Fundamentals.Console/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Iron is officially forged");
+
+int age = 30;
+double pi =  3.14159;
+string name = "Alex";
+bool isLearning = true;
+
+var year = 2026;
+
+Console.WriteLine($"Hello {name}, you are {age} years old");
+Console.WriteLine($"We are in {year} and Pi is {pi} ");
