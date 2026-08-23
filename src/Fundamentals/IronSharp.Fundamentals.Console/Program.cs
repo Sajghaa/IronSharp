d3b3726d005d7ecc442e7e1dded9ca8a05236e31
Serge <sajghaa@gmail.com>
2026-08-23 23:14:50 +0200

    Updated new file

--- src/Fundamentals/IronSharp.Fundamentals.Console/Program.cs
+++ src/Fundamentals/IronSharp.Fundamentals.Console/Program.cs
@@ -9,3 +9,24 @@ var year = 2026;
 
 Console.WriteLine($"Hello {name}, you are {age} years old");
 Console.WriteLine($"We are in {year} and Pi is {pi} ");
+
+if (age >= 18)
+{
+    Console.WriteLine($"{name} is an adult.");
+}
+else
+{
+    Console.WriteLine($"{name} is a minor");
+}
+
+string weather = "Sunny";
+string action = weather switch
+{
+    "Sunny" => "Go outside",
+    "Rainy" => "Take an umbrella",
+    "Cloudy" => "Bring a jacket",
+    _=> "Check the forecast"    
+};
+
+Console.WriteLine($"Weather advice: {action}");
+
