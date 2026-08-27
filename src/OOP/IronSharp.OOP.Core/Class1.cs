584f6895aea6bb67d91314c39a1f2485f48c20a8
Serge <sajghaa@gmail.com>
2026-08-27 22:17:11 +0200

    Added src/OOP/IronSharp.OOP.Core/Class1.cs

--- src/OOP/IronSharp.OOP.Core/Class1.cs
+++ src/OOP/IronSharp.OOP.Core/Class1.cs
@@ -1,6 +1,36 @@
 ﻿namespace IronSharp.OOP.Core;
 
-public class Class1
+public static class Calculator
 {
 
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int SumAll(params int[] numbers)
+    {
+        int total = 0;
+        foreach (int num in numbers)
+        {
+            total += num;
+        }
+        return total;
+    }
+
+    public static string FormatResult(int value, string prefix = "Result: ")
+    {
+        return $"{prefix}{value}";
+    }
+
+    public static bool TryDivide(int dividend, int divisor, out int result)
+    {
+        if (divisor == 0)
+        {
+            result = 0;
+            return false;
+        }
+        result = dividend / divisor;
+        return true;
+    }
 }
